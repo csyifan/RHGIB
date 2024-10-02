f6477cbc79f5a7b7e917953c702ca401c64ede1d
yifan <82603067+csyifan@users.noreply.github.com>
2024-10-02 13:59:22 +0800

    Add files via upload

---
+++ util/data_process.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+
+def sp_to_spt(mat):
+    coo = mat.tocoo()
+    values = coo.data
+    indices = np.vstack((coo.row, coo.col))
+
+    i = torch.LongTensor(indices)
+    v = torch.FloatTensor(values)
+    shape = coo.shape
+
+    return torch.sparse.FloatTensor(i, v, torch.Size(shape))
+
+
+def mat2tensor(mat):
+    if type(mat) is np.ndarray:
+        return torch.from_numpy(mat).type(torch.FloatTensor)
+    return sp_to_spt(mat)
+
